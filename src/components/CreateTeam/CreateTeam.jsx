f3cf77c29966250ebf8ca26fd77a3bb5bb0e0d66
Veronika Metodieva <veronikamm@abv.bg>
2023-06-01 15:43:03 +0300

    refactor: Break down SideBar to smalled components
    Create TeamButton and CreateTeam

---
+++ src/components/CreateTeam/CreateTeam.jsx
@@ -0,0 +1,137 @@
+import { Box, IconButton, Tooltip, Input, FormControl, FormLabel, FormHelperText, HStack, Popover, PopoverContent, PopoverTrigger, PopoverArrow, PopoverBody, PopoverHeader, PopoverCloseButton, Button, Portal } from '@chakra-ui/react';
+import { TriangleDownIcon } from '@chakra-ui/icons';
+import { useContext, useRef, useState } from 'react';
+import { useToast } from '@chakra-ui/react';
+import { TEAM_NAME_MAX_LENGTH, TEAM_NAME_MIN_LENGTH } from '../../constants/constants';
+import { getTeamByName, createTeam } from '../../services/teams.service';
+import { storeImage } from '../../services/image.service';
+import { AuthContext } from '../../context/authContext';
+import { createGeneralChannel } from '../../services/channels.service';
+
+const CreateTeam = () => {
+    const toast = useToast();
+    const nameInput = useRef(null);
+    const avatarInput = useRef();
+    const [form, setForm] = useState({
+        teamName: '',
+        teamAvatar: '',
+    });
+    const { user } = useContext(AuthContext);
+
+    const updateForm = prop => e => {
+        setForm({ ...form, [prop]: e.target.value });
+    };
+
+    const updateAvatar = e => {
+        setForm({ ...form, teamAvatar: e.target.files[0] });
+    };
+
+    const onCreate = async (e) => {
+        try {
+            e.preventDefault();
+            if (form.teamName.length < TEAM_NAME_MIN_LENGTH || form.teamName.length > TEAM_NAME_MAX_LENGTH) {
+                throw new Error(`Team name should be between ${TEAM_NAME_MIN_LENGTH} and ${TEAM_NAME_MAX_LENGTH} symbols!`);
+            }
+
+            const teamSnapshot = await getTeamByName(form.teamName);
+            if (teamSnapshot.exists())
+                throw new Error('Team name is already taken!');
+
+            const imgUrl = form.teamAvatar ? await storeImage(form.teamAvatar, form.teamName) : '';
+            const teamId = await createTeam(form.teamName, imgUrl, user.uid);
+            await createGeneralChannel(teamId);
+
+            nameInput.current.value = null;
+            avatarInput.current.value = null;
+
+            toast({
+                title: 'Team was created!',
+                status: 'success',
+                duration: 3000,
+                isClosable: true,
+                position: 'top-left'
+            });
+        } catch (error) {
+            toast({
+                title: error.message,
+                status: 'error',
+                duration: 3000,
+                isClosable: true,
+                position: 'top-left'
+            });
+        }
+    };
+
+    return (<Popover initialFocusRef={nameInput} onClose={() => {
+        setForm({
+            teamName: '',
+            teamAvatar: '',
+        });
+        nameInput.current.value = null;
+        avatarInput.current.value = null;
+    }} closeOnBlur>
+        <PopoverTrigger>
+            <span><Tooltip label="Add Team" placement="right">
+                <IconButton
+                    icon={<TriangleDownIcon />}
+                    aria-label='create team'
+                    bg='accent'
+                    borderRadius="50%"
+                    size="lg"
+                    _hover={{ bg: 'primaryLight', color: 'primaryDark' }}
+                /></Tooltip></span>
+        </PopoverTrigger>
+        <Portal >
+            <PopoverContent textAlign="center" bg='primaryLight'>
+                <PopoverArrow />
+                <PopoverHeader as="h2" bg="primary" fontWeight="bold" letterSpacing={2}>CREATE&nbsp; A&nbsp; NEW&nbsp; TEAM</PopoverHeader>
+                <PopoverCloseButton />
+                <PopoverBody>
+                    <form onSubmit={onCreate}>
+                        <FormControl display="flex" flexDirection="column" gap="1rem">
+                            <Box>
+                                <FormLabel >Team Name*</FormLabel>
+                                <Input
+                                    id="team-name"
+                                    ref={nameInput}
+                                    type="text"
+                                    bg="tertiary"
+                                    value={form.teamName}
+                                    onChange={updateForm('teamName')}
+                                    autoComplete="off"
+                                    placeholder="Team Name"
+                                    required></Input>
+                                <FormHelperText color="primaryDark">Name should be between 3 and 40 symbols</FormHelperText>
+                            </Box>
+                            <Box>
+                                <FormLabel >Team Avatar</FormLabel>
+                                <Input
+                                    id="team-avatar"
+                                    ref={avatarInput}
+                                    type="file"
+                                    bg="tertiary"
+                                    pt="4px"
+                                    borderRadius="md"
+                                    onChange={updateAvatar}
+                                    autoComplete="off"
+                                    accept='.jpg,.png,.jpeg' />
+                            </Box>
+                            <HStack justify="space-evenly">
+                                <Button
+                                    w="100%"
+                                    border="1px"
+                                    borderColor="primaryDark"
+                                    bg="accent"
+                                    type="submit"
+                                    _hover={{ color: 'primaryDark' }}>
+                                    CREATE</Button>
+                            </HStack>
+                        </FormControl>
+                    </form>
+                </PopoverBody>
+            </PopoverContent>
+        </Portal>
+    </Popover>);
+};
+
+export default CreateTeam;
