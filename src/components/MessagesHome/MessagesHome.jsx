5d77e3e8e29a9ff18cd9a0c27d0a30fb21f3a468
Stoyan Velinov <stoyan.velinov@gmail.com>
2023-06-09 07:13:52 +0300

    Fix typo @MessagesHome

--- src/components/MessagesHome/MessagesHome.jsx
+++ src/components/MessagesHome/MessagesHome.jsx
@@ -2,25 +2,19 @@ import { ReactComponent as ChatImage } from '../../assets/svg/chat.svg';
 import {
   Box,
   Flex,
-  Button,
-  AbsoluteCenter,
   Center,
-  FormControl,
-  Textarea,
-  HStack,
 } from '@chakra-ui/react';
 
-const MessgesHome = () => {
+const MessagesHome = () => {
     return (
         <Flex fontSize="md" flexDirection="column">
           <Box position='relative' >
               <Center  p='10' color='white' axis='both'>
-                <ChatImage width='60%'
-  height='60%'/>
+                <ChatImage width='60%' height='60%'/>
               </Center>
             </Box>
         </Flex>
       );
 };
 
-export default MessgesHome;
+export default MessagesHome;
